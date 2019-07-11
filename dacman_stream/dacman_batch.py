7cf54e6880fe2ddfb181d3f7caf7f8fb80323f64
elbashandy <a.elbashandy@gmail.com>
2019-07-11 11:49:03 -0700

    adding new updates to dacman_batch

--- dacman_stream/dacman_batch.py
+++ dacman_stream/dacman_batch.py
@@ -4,6 +4,76 @@ import h5py
 import numpy as np
 import time
 
+data_pull_start = {}
+data_pull_end = {}
+job_end_processing = {}
+job_end_data_put = {}
+
+
+def write_to_csv():
+    output_dir_path = _config.OUTPUT_CSV_DIR
+
+    name = _config.CSV_DICTS_DIRS[0]
+    output_full_path = os.path.join(output_dir_path, name, 
+            '%s_%s_%s.csv' % (name, socket.gethostname(), os.getpid()))
+
+    with open(output_full_path, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in data_pull_start.items():
+            writer.writerow([key, value])
+
+    name = _config.CSV_DICTS_DIRS[1]
+    output_full_path = os.path.join(output_dir_path, name, 
+            '%s_%s_%s.csv' % (name, socket.gethostname(), os.getpid()))
+
+    with open(output_full_path, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in data_pull_end.items():
+            writer.writerow([key, value])
+
+    name = _config.CSV_DICTS_DIRS[2]
+    output_full_path = os.path.join(output_dir_path, name, 
+            '%s_%s_%s.csv' % (name, socket.gethostname(), os.getpid()))
+
+    with open(output_full_path, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in job_end_processing.items():
+            writer.writerow([key, value])
+
+    name = _config.CSV_DICTS_DIRS[3]
+    output_full_path = os.path.join(output_dir_path, name, 
+            '%s_%s_%s.csv' % (name, socket.gethostname(), os.getpid()))
+
+    with open(output_full_path, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in job_end_data_put.items():
+            writer.writerow([key, value])
+
+
+def two_frame_analysis(data_a, data_b):
+    rms_log = None
+    t_test_t = None
+    t_test_p = None
+    do_ttest = True
+
+    matrix1 = data_a
+    matrix2 = data_b
+
+    #matrix1 = dataA.flatten()
+    #matrix2 = dataB.flatten()
+
+    rms = sqrt(mean_squared_error(matrix1, matrix2)) #compute RMSE between the 2 input frames
+    min_d = min(min(matrix1), min(matrix2))
+    max_d = max(max(matrix1), max(matrix2))
+    
+    rms_log = np.log(rms) #logarithm of RMSE
+
+    if do_ttest:
+        #option 1: t-test over the whole frame
+        t_test_t, t_test_p = stats.ttest_ind(matrix1, matrix2, equal_var=True)
+    
+    return rms_log, t_test_t, t_test_p
+
 
 def get_change_pairs(queue_hash, job_list_hash, loopstep, source_dir, dataset, output_dir='.', streaming_time=5):
     mean_correct = False
@@ -65,33 +135,32 @@ def collection_diff_mpi():
     status = MPI.Status()
 
     class States():
-      READY = 0
-      START = 1
-      DONE = 2
-      EXIT = 3
+        READY = 0
+        START = 1
+        DONE = 2
+        EXIT = 3
 
     if rank == 0:        
         change_pair_num = 0
         closed_workers = 0
         num_workers = size - 1
       
-        change_pairs = get_change_pairs()
+        for change_pair in get_change_pairs():
+            result = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+            source = statsu.Get_source()
+            tag = status.Get_tag()
+    
+            if tag == States.READY:
+                comm.send(change_pair, dest=source, tag=States.START)
 
         while closed_workers < num_workers:
-            for change_pair in get_change_pairs():
-                result = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
-                source = statsu.Get_source()
-                tag = status.Get_tag()
-        
-                if tag == States.READY:
-                    comm.send(change_pair, dest=source, tag=States.START)
-                    change_pair_num += 1
-                elif tag == States.DONE:
-                    pass
-                elif tag == States.EXIT:
-                    closed_workers += 1
-            else:
-                comm.send(None, dest=MPI.ANY_SOURCE, tag=States.EXIT)
+            result = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+            source = statsu.Get_source()
+            tag = status.Get_tag()
+
+            comm.send(None, dest=MPI.ANY_SOURCE, tag=States.EXIT)
+            if tag == States.EXIT:
+                closed_workers += 1
     else:
         while True:
             comm.send(None, dest=0, tag=States.READY)
@@ -101,8 +170,7 @@ def collection_diff_mpi():
             if tag == States.START:
                 data_a = change_pair[0]
                 data_b = change_pair[1]
-                file_diff(data_a, data_b)
-                comm.send(None, dest=0, tag=States.DONE)
+                two_frame_analysis(data_a, data_b)
             elif tag == States.EXIT:
                 comm.send(None, dest=0, tag=States.EXIT)
                 break
