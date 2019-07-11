fd37cae7cbe5f12836c4944e040f3b8393733e5c
Abdelrahman Elbashandy <aaelbashandy@lbl.gov>
2019-07-11 16:47:37 -0700

    Added more functionality to dacman_batch.py

--- dacman_stream/dacman_batch.py
+++ dacman_stream/dacman_batch.py
@@ -3,46 +3,80 @@ import sys
 import h5py
 import numpy as np
 import time
+import uuid
 
+from mpi4py import MPI
+
+data_send_start = {}
+data_send_end = {}
 data_pull_start = {}
 data_pull_end = {}
 job_end_processing = {}
 job_end_data_put = {}
 
+TASK_PREFIX = "task"
+CSV_DICTS_DIRS = [
+    "data_send_start",
+    "data_send_end",
+    "data_pull_start",
+    "data_pull_end",
+    "job_end_processing",
+    "job_end_data_put"
+]
+
+OUTPUT_CSV_DIR = "/global/homes/e/elbashan/workspace/dac-man/sandbox/job_finished_timestamp_dir/"
 
 def write_to_csv():
-    output_dir_path = _config.OUTPUT_CSV_DIR
+    output_dir_path = OUTPUT_CSV_DIR
 
-    name = _config.CSV_DICTS_DIRS[0]
-    output_full_path = os.path.join(output_dir_path, name, 
-            '%s_%s_%s.csv' % (name, socket.gethostname(), os.getpid()))
+    name = CSV_DICTS_DIRS[0]
+    output_full_path = os.path.join(output_dir_path, 
+            '%s.csv' % (name))
+
+    with open(output_full_path, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in data_send_start.items():
+            writer.writerow([key, value])
+
+    name = CSV_DICTS_DIRS[1]
+    output_full_path = os.path.join(output_dir_path, 
+            '%s.csv' % (name))
+
+    with open(output_full_path, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in data_send_end.items():
+            writer.writerow([key, value])
+
+    name = CSV_DICTS_DIRS[2]
+    output_full_path = os.path.join(output_dir_path, 
+            '%s.csv' % (name))
 
     with open(output_full_path, 'w') as csv_file:
         writer = csv.writer(csv_file)
         for key, value in data_pull_start.items():
             writer.writerow([key, value])
 
-    name = _config.CSV_DICTS_DIRS[1]
-    output_full_path = os.path.join(output_dir_path, name, 
-            '%s_%s_%s.csv' % (name, socket.gethostname(), os.getpid()))
+    name = CSV_DICTS_DIRS[3]
+    output_full_path = os.path.join(output_dir_path, 
+            '%s.csv' % (name))
 
     with open(output_full_path, 'w') as csv_file:
         writer = csv.writer(csv_file)
         for key, value in data_pull_end.items():
             writer.writerow([key, value])
 
-    name = _config.CSV_DICTS_DIRS[2]
-    output_full_path = os.path.join(output_dir_path, name, 
-            '%s_%s_%s.csv' % (name, socket.gethostname(), os.getpid()))
+    name = CSV_DICTS_DIRS[4]
+    output_full_path = os.path.join(output_dir_path, 
+            '%s.csv' % (name))
 
     with open(output_full_path, 'w') as csv_file:
         writer = csv.writer(csv_file)
         for key, value in job_end_processing.items():
             writer.writerow([key, value])
 
-    name = _config.CSV_DICTS_DIRS[3]
-    output_full_path = os.path.join(output_dir_path, name, 
-            '%s_%s_%s.csv' % (name, socket.gethostname(), os.getpid()))
+    name = CSV_DICTS_DIRS[5]
+    output_full_path = os.path.join(output_dir_path, 
+            '%s.csv' % (name))
 
     with open(output_full_path, 'w') as csv_file:
         writer = csv.writer(csv_file)
@@ -75,13 +109,13 @@ def two_frame_analysis(data_a, data_b):
     return rms_log, t_test_t, t_test_p
 
 
-def get_change_pairs(queue_hash, job_list_hash, loopstep, source_dir, dataset, output_dir='.', streaming_time=5):
+def get_change_pairs(dataset, output_dir='.'):
     mean_correct = False
     use_gaussian_filter = False
     do_ttest = True
     add_to_fix_log=False
 
-    fx = h5py.File(os.path.join(source_dir, dataset), 'r')
+    fx = h5py.File(dataset, 'r')
 
     for group in fx:
         print("group: " + str(group))
@@ -93,7 +127,6 @@ def get_change_pairs(queue_hash, job_list_hash, loopstep, source_dir, dataset, o
 
             print("n_frames:", n_frames)
 
-            t_end = time.time() + 60 * streaming_time
             ii = 0
 
             filename1 = "/%s/%s/%s" % (group, subgroup, frames_list[ii])
@@ -119,7 +152,8 @@ def get_change_pairs(queue_hash, job_list_hash, loopstep, source_dir, dataset, o
                 log_mat_pos2 = dx2[:frame_len]
                 matrix2 = log_mat_pos2.flatten()
 
-                yield (matrix_temp, matrix2)
+                task_uuid = "%s:%s" % (TASK_PREFIX, str(uuid.uuid4()))
+                yield (task_uuid, matrix_temp, matrix2)
                 
                 matrix_temp = np.copy(matrix2)
                 ii += 1
@@ -151,26 +185,45 @@ def collection_diff_mpi():
             tag = status.Get_tag()
     
             if tag == States.READY:
+                task_uuid = change_pair[0]
+                data_send_start[task_uuid] = time.time()
                 comm.send(change_pair, dest=source, tag=States.START)
+                data_send_end[task_uuid] = time.time()
+                print("Rank:", str(rank), "pushed", task_uuid, "to Worker:", str(source))
 
         while closed_workers < num_workers:
             result = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
-            source = statsu.Get_source()
+            source = status.Get_source()
             tag = status.Get_tag()
 
             comm.send(None, dest=MPI.ANY_SOURCE, tag=States.EXIT)
             if tag == States.EXIT:
                 closed_workers += 1
+
+        # Saving Results to CSV
+        write_to_csv()
     else:
         while True:
             comm.send(None, dest=0, tag=States.READY)
+            pull_start = time.time()
             change_pair = comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
+            pull_end = time.time()
             tag = status.Get_tag()
 
             if tag == States.START:
-                data_a = change_pair[0]
-                data_b = change_pair[1]
+                task_uuid = change_pair[0]
+                data_a = change_pair[1]
+                data_b = change_pair[2]
+
+                data_pull_start[task_uuid] = pull_start
+                data_pull_end[task_uuid] = pull_end
+                
                 two_frame_analysis(data_a, data_b)
+                job_end_processing[task_uuid] = time.time()
+                comm.send(None, dest=0, tag=States.DONE)
+                job_end_data_put[task_uuid] = time.time()
+
+                print("Rank:", str(rank), "processed", task_uuid)
             elif tag == States.EXIT:
                 comm.send(None, dest=0, tag=States.EXIT)
                 break
