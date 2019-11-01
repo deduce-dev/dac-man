77574af31808fdee47daac105a4c3ba14cf338ab
sspoon <sarahpoon@Sarahs-MBP-2.attlocal.net>
2019-11-01 09:58:42 -0700

    adding dacman test

--- dacman/ui/server/server.py
+++ dacman/ui/server/server.py
@@ -1,6 +1,9 @@
 # server.py
 from flask import Flask, render_template, json
 from astropy.io import fits
+from dacman import Executor, DataDiffer
+from dacman.plugins.default import DefaultPlugin
+
 app = Flask(__name__, static_folder='../fits/build/static', template_folder='../fits/build')
 
 @app.route('/')
@@ -16,15 +19,21 @@ def api_fits_test():
 		'output' : output
 	})
 
+@app.route('/api/dacmantest')
+def api_dacman_test():
+	file1 = '../../../examples/data/simple/v0/file1'
+	file2 = '../../../examples/data/simple/v1/file1'
+	comparisons = [(file1, file2)]
+	differ = DataDiffer(comparisons, executor=Executor.DEFAULT)
+	differ.use_plugin(DefaultPlugin)
+	results = differ.start()
+	print(results)
+	return json.jsonify(results)
+
 @app.route('/api/fitsinfo')
 def api_fits_info():
 	hdul = fits.open('../spCFrame-b1-00161868.fits')
 	output = hdul.info(output=False)
-	#print("output is")
-	#print(output)
-	#return json.jsonify({
-	#	'output' : output
-	#})
 	return json.jsonify(output)
 
 @app.route('/hello')
