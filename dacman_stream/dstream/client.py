9a9d10de8dceab56b08c657951c422a2653b14d1
dghoshal-lbl <dghoshal@lbl.gov>
2020-02-12 02:38:09 -0500

    multi-src generic client

--- dacman_stream/dstream/client.py
+++ dacman_stream/dstream/client.py
@@ -18,6 +18,7 @@ from cache import Cache
 import pandas as pd
 import argparse
 import sys
+from datetime import datetime
 
 
 def send(cache, k, datablock, window_size):
@@ -34,12 +35,9 @@ def get_window_key(row, key_name):
     return window_key
 
 
-def main(stream_src, measurement, window_size, key_name):
+def main(stream_transformer, stream_src, measurement, window_size, key_name):
     cache = Cache()
-    df = pd.read_csv(stream_src, comment='#', sep=',', na_filter=False, dtype='str')
-    #m_series = df[measurement]
-    #for m in m_series.values:
-    for index, row in df.head().iterrows():
+    for row in stream_transformer(stream_src):
         window_key = get_window_key(row, key_name)
         print(window_key, row[measurement])
         send(cache, window_key, row[measurement], window_size)
@@ -48,16 +46,18 @@ def main(stream_src, measurement, window_size, key_name):
 ###################
 def transform_fluxnet_stream(stream_src):
     df = pd.read_csv(stream_src, comment='#', sep=',', na_filter=False, dtype='str')
-    df['datetime'] = pd.to_datetime(df['TIMESTAMP_END'], format="%Y%m%d%H%M",errors='coerce')
-    for index, row in df.head().iterrows():
+    #df['datetime'] = pd.to_datetime(df['TIMESTAMP_END'], format="%Y%m%d%H%M",errors='coerce')
+    df['datetime'] = df['TIMESTAMP_END']
+    for index, row in df.iterrows():
         yield row
 
 
 def transform_lathuile_stream(stream_src):
     df = pd.read_csv(stream_src, comment='#', sep=',', na_filter=False, dtype='str')
-    df['datetime'] = pd.to_datetime(df['Year'].str[:-2]) + pd.to_timedelta(df['DoY'].str[:-2] + 'days') + \
-        pd.to_timedelta('-1 day') + pd.to_timedelta(pd.to_numeric(df['Time']), unit='h')
-    for index, row in df.head().iterrows():
+    df['datetime'] = (pd.to_datetime(df['Year'].str[:-2]) + pd.to_timedelta(df['DoY'].str[:-2] + 'days') + \
+        pd.to_timedelta('-1 day') + pd.to_timedelta(pd.to_numeric(df['Time']), unit='h'))
+    df['datetime'] = df['datetime'].dt.strftime('%Y%m%d%H%M')
+    for index, row in df.iterrows():
         yield row
 
 
@@ -67,11 +67,12 @@ def _fluxnetClientParser(subparsers):
                                           formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                           help=""" Streaming dstream for Fluxnet data """)
 
+    parser_worker.set_defaults(action="fluxnet")
     parser_worker.add_argument(dest='filename', help='input stream file')
     parser_worker.add_argument('-m', '--measurement',
                                choices=['CO2_F_MDS', 'WD', 'TA_F'], type=str,
                                default='CO2_F_MDS', help='measurement')
-    parser_worker.add_argument('-k', '--keyname', type=str, help='key name', default='TIMESTAMP_START')
+    parser_worker.add_argument('-k', '--keyname', type=str, help='key name', default='datetime')
     parser_worker.add_argument('-s', '--windowsize', type=int, help='window size', default=1)
 
 
@@ -80,11 +81,12 @@ def _lathuileClientParser(subparsers):
                                           formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                           help=""" Streaming dstream for La Thuile data """)
 
+    parser_worker.set_defaults(action="lathuile")
     parser_worker.add_argument(dest='filename', help='input stream file')
     parser_worker.add_argument('-m', '--measurement',
-                               choices=['CO2_F_MDS', 'WD', 'TA_F'], type=str,
-                               default='CO2_F_MDS', help='measurement')
-    parser_worker.add_argument('-k', '--keyname', type=str, help='key name', default='TIMESTAMP_START')
+                               choices=['CO2', 'WD', 'Ta_f'], type=str,
+                               default='CO2', help='measurement')
+    parser_worker.add_argument('-k', '--keyname', type=str, help='key name', default='datetime')
     parser_worker.add_argument('-s', '--windowsize', type=int, help='window size', default=1)
 
 
@@ -93,17 +95,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="",
                                      prog="dstream",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument(dest='filename', help='input stream file')
-    parser.add_argument('-m', '--measurement',
-                        choices=['CO2_F_MDS', 'WD', 'TA_F'], type=str,
-                        default='CO2_F_MDS', help='measurement')
-    parser.add_argument('-k', '--keyname', type=str, help='key name', default='TIMESTAMP_START')
-    parser.add_argument('-s', '--windowsize', type=int, help='window size', default=1)
+
+    subparsers = parser.add_subparsers()
+    _fluxnetClientParser(subparsers)
+    _lathuileClientParser(subparsers)
 
     args = parser.parse_args()
     if len(args.__dict__) == 0:
         parser.print_usage()
         sys.exit(1)
 
-    main(args.filename, args.measurement, args.windowsize)
-    # python dstream.py data/fluxnet2015/FLX_AT-Neu_FLUXNET2015_FULLSET_HH_2002-2012_1-3.csv -s 2
+    if args.action == 'fluxnet':
+        fn = transform_fluxnet_stream
+    else:
+        fn = transform_lathuile_stream
+
+    main(fn, args.filename, args.measurement, args.windowsize, args.keyname)
+    # python dstream.py fluxnet data/fluxnet2015/FLX_AT-Neu_FLUXNET2015_FULLSET_HH_2002-2012_1-3.csv -s 2
