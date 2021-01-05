616b85e2e68e950812e96d707ebe05219edd663d
Abdelrahman Elbashandy <aaelbashandy@lbl.gov>
2021-01-05 14:33:56 -0800

    Now the flagging R script supports flagging Bad/Out-of-range values

--- dacman/ui/server/flagging/r_scripts/qa_n_s.R
+++ dacman/ui/server/flagging/r_scripts/qa_n_s.R
@@ -1,3 +1,5 @@
+#!/usr/bin/env Rscript
+
 ##########
 # TITLE: QA for DETECTING AND FLAGGING NAs, DUPLICATES, OUTLIERS AND EXTREME VALUES IN TIME SERIES
 ##########
@@ -8,6 +10,38 @@ rm (list = ls())
 library (vctrs)
 library (rstatix)
 library(optparse)
+
+
+option_list = list(
+    make_option(c("-f", "--file"), type="character", default=NULL, 
+              help="dataset file name", metavar="character"),
+    make_option(c("-o", "--output"), type="character", default=NULL,
+              help="output file name [default= %default]", metavar="character"),
+    make_option(c("-v", "--variable"), type="character", default=NULL,
+              help="variables to flag", metavar="character"),
+    make_option(c("--is_numeric"), action = "store_true", default = FALSE,
+              help="Check if the variable has numeric values [default= %default]"),
+    make_option(c("--is_date"), action = "store_true", default = FALSE,
+              help="Check if the variable is date [default= %default]"),
+    make_option(c("-n", "--check_null"), action = "store_true", default = FALSE,
+              help="Check NULL values [default= %default]"),
+    make_option(c("-d", "--check_dupl"), action = "store_true", default = FALSE,
+              help="Check duplicate values"),
+    make_option(c("-s", "--subsequent"), type = "integer", default = 3,
+              help="Number of subsequent values to be considered duplicate",
+              metavar = "number"),
+    make_option(c("-b", "--bad"), type="character", default=NULL, 
+              help="comma separated range 'from,to'. e.g '0,100'", metavar="character"),
+    make_option(c("-t", "--out_iqr"), type="numeric", default=0, 
+              help="IQR coefficient for outlier values detection", metavar="character"),
+    make_option(c("-e", "--extr_iqr"), type="numeric", default=0, 
+              help="IQR coefficient for extreme values detection", metavar="character")
+    #make_option(c("-v", "--variable"), type="character", default=NULL, 
+    #          help="comma separated list of variables to flag", metavar="character")
+)
+ 
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
 
 
 read_variable <- function(dataset, var_name, is_numeric=TRUE) {
@@ -67,10 +101,10 @@ detect_na_dupl <- function(var) {
 }
 
 detect_dupl_numeric <- function(var, n=3) {
-    cat(sprintf("Numeric Variable has duplicates: %s\n", vec_duplicate_any(variable)))
+    cat(sprintf("Numeric Variable has duplicates: %s\n", vec_duplicate_any(var)))
 
     # Compute the lengths and values of runs of equal values in a vector using 'rle'--'Run Length Encoding'
-    rl <- rle(variable)
+    rl <- rle(var)
     rp = rep(rl$lengths != n, times=rl$lengths)
 
     variable.Dupl = ifelse(rp=="TRUE", var, 'DUPL')
@@ -81,7 +115,7 @@ detect_dupl_numeric <- function(var, n=3) {
 detect_na_dupl_numeric <- function(var, n=3) {
     # IDENTIFY IF THERE ARE DUPLICATED DATES/TIMES (using library {vctrs} )
     #cat("Numeric Variable has duplicates: ", vec_duplicate_any(variable))
-    cat(sprintf("Numeric Variable has duplicates: %s\n", vec_duplicate_any(var)))
+    cat(sprintf("Numeric NA Variable has duplicates: %s\n", vec_duplicate_any(var)))
 
     # Compute the lengths and values of runs of equal values in a vector using 'rle'--'Run Length Encoding'
     rl <- rle(var)
@@ -92,6 +126,13 @@ detect_na_dupl_numeric <- function(var, n=3) {
     variable.NaDupl = ifelse (rp == 'NA', 1, ifelse(rp=="TRUE", 0, 'DUPL'))
 
     return(variable.NaDupl)
+}
+
+
+detect_out_of_range <- function(var) {
+    variable.Bad = ifelse(var < bad_vals_start, 1, ifelse(var > bad_vals_end, 2, 0))
+
+    return(variable.Bad)
 }
 
 ################################################
@@ -133,50 +174,85 @@ detect_outliers_extremes <- function(var, outlier_iqr_coef=1.5, extreme_iqr_coef
 # SAVING FLAGGED RESULTS TO FILE ----
 ##################################################
 
-save_file <- function(var_name, var, flag_var, flag_outl, flag_extr) {
+save_file <- function(var_name, var, flag_var, flag_bad, flag_outl, flag_extr, output=NULL) {
     df <- NULL
-    if (flag_var == NULL && flag_outl == NULL && flag_extr == NULL) {
+    if (is.null(flag_var) && is.null(flag_bad) && is.null(flag_outl) && is.null(flag_extr)) {
         stop("Variable didn't go through any Flagging process")
-    } else if (flag_var == NULL && flag_outl == NULL) {
+    } else if (is.null(flag_var) && is.null(flag_bad) && is.null(flag_outl)) {
         df = data.frame(var, flag_extr)
         names(df) <- c(
                         var_name,
                         paste("flag_extr", var_name, sep = "_")
                     )
-    } else if (flag_var == NULL && flag_extr == NULL) {
+    } else if (is.null(flag_var) && is.null(flag_bad) && is.null(flag_extr)) {
         df = data.frame(var, flag_outl)
         names(df) <- c(
                         var_name,
                         paste("flag_outl", var_name, sep = "_")
                     )
-    } else if (flag_outl == NULL && flag_extr == NULL) {
+    } else if (is.null(flag_var) && is.null(flag_outl) && is.null(flag_extr)) {
+        df = data.frame(var, flag_bad)
+        names(df) <- c(
+                        var_name,
+                        paste("flag_bad", var_name, sep = "_")
+                    )
+    } else if (is.null(flag_bad) && is.null(flag_outl) && is.null(flag_extr)) {
         df = data.frame(var, flag_var)
         names(df) <- c(
                         var_name,
                         paste("flag", var_name, sep = "_")
                     )
-    } else if (flag_var == NULL) {
+    } else if (is.null(flag_var) && is.null(flag_bad)) {
         df = data.frame(var, flag_outl, flag_extr)
         names(df) <- c(
                         var_name,
                         paste("flag_outl", var_name, sep = "_"),
                         paste("flag_extr", var_name, sep = "_")
                     )
-    } else if (flag_outl == NULL) {
+    } else if (is.null(flag_var) && is.null(flag_outl)) {
+        df = data.frame(var, flag_bad, flag_extr)
+        names(df) <- c(
+                        var_name,
+                        paste("flag_bad", var_name, sep = "_"),
+                        paste("flag_extr", var_name, sep = "_")
+                    )
+    } else if (is.null(flag_var) && is.null(flag_extr)) {
+        df = data.frame(var, flag_bad, flag_outl)
+        names(df) <- c(
+                        var_name,
+                        paste("flag_bad", var_name, sep = "_"),
+                        paste("flag_outl", var_name, sep = "_")
+                    )
+    } else if (is.null(flag_bad) && is.null(flag_outl)) {
         df = data.frame(var, flag_var, flag_extr)
         names(df) <- c(
                         var_name,
                         paste("flag", var_name, sep = "_"),
                         paste("flag_extr", var_name, sep = "_")
                     )
-    } else if (flag_extr == NULL) {
+    } else if (is.null(flag_bad) && is.null(flag_extr)) {
         df = data.frame(var, flag_var, flag_outl)
         names(df) <- c(
                         var_name,
                         paste("flag", var_name, sep = "_"),
                         paste("flag_outl", var_name, sep = "_")
                     )
-    } else {
+    } else if (is.null(flag_outl) && is.null(flag_extr)) {
+        df = data.frame(var, flag_var, flag_bad)
+        names(df) <- c(
+                        var_name,
+                        paste("flag", var_name, sep = "_"),
+                        paste("flag_bad", var_name, sep = "_")
+                    )
+    } else if (is.null(flag_var)) {
+        df = data.frame(var, flag_bad, flag_outl, flag_extr)
+        names(df) <- c(
+                        var_name,
+                        paste("flag_bad", var_name, sep = "_"),
+                        paste("flag_outl", var_name, sep = "_"),
+                        paste("flag_extr", var_name, sep = "_")
+                    )
+    } else if (is.null(flag_bad)) {
         df = data.frame(var, flag_var, flag_outl, flag_extr)
         names(df) <- c(
                         var_name,
@@ -184,67 +260,61 @@ save_file <- function(var_name, var, flag_var, flag_outl, flag_extr) {
                         paste("flag_outl", var_name, sep = "_"),
                         paste("flag_extr", var_name, sep = "_")
                     )
+    } else if (is.null(flag_outl)) {
+        df = data.frame(var, flag_var, flag_bad, flag_extr)
+        names(df) <- c(
+                        var_name,
+                        paste("flag", var_name, sep = "_"),
+                        paste("flag_bad", var_name, sep = "_"),
+                        paste("flag_extr", var_name, sep = "_")
+                    )
+    } else if (is.null(flag_extr)) {
+        df = data.frame(var, flag_var, flag_bad, flag_outl)
+        names(df) <- c(
+                        var_name,
+                        paste("flag", var_name, sep = "_"),
+                        paste("flag_bad", var_name, sep = "_"),
+                        paste("flag_outl", var_name, sep = "_")
+                    )
+    } else {
+        df = data.frame(var, flag_var, flag_bad, flag_outl, flag_extr)
+        names(df) <- c(
+                        var_name,
+                        paste("flag", var_name, sep = "_"),
+                        paste("flag_bad", var_name, sep = "_"),
+                        paste("flag_outl", var_name, sep = "_"),
+                        paste("flag_extr", var_name, sep = "_")
+                    )
     }
 
-    write.csv(df, paste(paste("QA_flagging_", var_name, sep = ""), ".csv", sep=""))
+    write.csv(df, output, row.names=FALSE)
 }
 
 
 #####################################################################################
 
 
-option_list = list(
-    make_option(c("-f", "--file"), type="character", default=NULL, 
-              help="dataset file name", metavar="character"),
-    make_option(c("-o", "--out"), type="character", default="QA_flagging.csv",
-              help="output file name [default= %default]", metavar="character"),
-    make_option(c("-v", "--variable"), type="character",
-              help="variables to flag", metavar="character"),
-    make_option(c("--is_numeric"), action = "store_true", default = FALSE,
-              help="Check if the variable has numeric values [default= %default]")
-    make_option(c("--is_date"), action = "store_true", default = FALSE,
-              help="Check if the variable is date [default= %default]")
-    make_option(c("-n", "--check_null"), action = "store_true", default = FALSE,
-              help="Check NULL values [default= %default]")
-    make_option(c("-d", "--check_dupl"), action = "store_true", default = FALSE,
-              help="Check duplicate values")
-    make_option(c("-s", "--subsequent"), type = "integer", default = 3,
-              help="Number of subsequent values to be considered duplicate",
-              metavar = "number")
-    make_option(c("-b", "--bad"), type="character", default=NULL, 
-              help="comma separated range 'from,to'. e.g '0,100'", metavar="character")
-    make_option(c("-t", "--out_iqr"), type="numeric", default=0, 
-              help="IQR coefficient for outlier values detection", metavar="character")
-    make_option(c("-e", "--extr_iqr"), type="numeric", default=0, 
-              help="IQR coefficient for extreme values detection", metavar="character")
-    #make_option(c("-v", "--variable"), type="character", default=NULL, 
-    #          help="comma separated list of variables to flag", metavar="character")
-)
- 
-opt_parser = OptionParser(option_list=option_list)
-opt = parse_args(opt_parser)
-
-if (opt$file == NULL) {
+if (is.null(opt$file)) {
     stop("An input file needs to be set")
-}
-
-if (opt$out == NULL) {
-    stop("An output filename needs to be set")
 }
 
 if (opt$is_numeric == TRUE && opt$is_date == TRUE) {
     stop("Either the Numeric flag or the Date flag can be set")
 }
 
-if (opt$variable == NULL) {
+if (is.null(opt$variable)) {
     stop("A variable name needs to be specified")
 }
 
 
 bad_vals_range <- NULL
+bad_vals_start <- NULL
+bad_vals_end <- NULL
 
-if (opt$bad != NULL) {
+if (!is.null(opt$bad)) {
     bad_vals_range <- strsplit(opt$bad, ",")
+    bad_vals_start <- as.numeric(bad_vals_range[[1]][1])
+    bad_vals_end <- as.numeric(bad_vals_range[[1]][2])
 }
 
 #x = read.csv(file = '/project/QA_no_seasonal/AirportData_2008-2019_v3.csv')
@@ -267,19 +337,24 @@ if (opt$is_numeric) {
 
 flag_var <- NULL
 
-if (opt$check_null && opt&check_dupl && is_numeric) {
+if (opt$check_null && opt$check_dupl && opt$is_numeric) {
     flag_var <- detect_na_dupl_numeric(Var, n=opt$subsequent)
-} else if (opt$check_null && opt&check_dupl) {
+} else if (opt$check_null && opt$check_dupl) {
     flag_var <- detect_na_dupl(Var)
-} else if (opt&check_dupl && is_numeric) {
+} else if (opt$check_dupl && opt$is_numeric) {
     flag_var <- detect_dupl_numeric(Var, n=opt$subsequent)
-} else if (opt&check_dupl) {
+} else if (opt$check_dupl) {
     flag_var <- detect_dupl(Var)
 } else if (opt$check_null) {
     flag_var <- detect_na(Var)
 }
 
-Flag <- NULL
+flag_bad <- NULL
+
+if (!is.null(bad_vals_range)) {
+    flag_bad = ifelse(Var < bad_vals_start, 1, ifelse(Var > bad_vals_end, 2, 0))
+}
+
 Flag.outl <- NULL
 Flag.extr <- NULL
 
@@ -297,9 +372,10 @@ if (opt$out_iqr != 0 && opt$extr_iqr != 0) {
     Flag.extr <- detect_outliers(Var, iqr_coef=opt$extr_iqr)
 }
 
-save_file(var_name, Var, flag_var, Flag.outl, Flag.extr)
-
-head (df)
-summary (df)
-
+if (is.null(opt$output)) {
+    out <- paste(paste("QA_flagging_", var_name, sep = ""), ".csv", sep="")
+    save_file(var_name, Var, flag_var, flag_bad, Flag.outl, Flag.extr, output=out)
+} else {
+    save_file(var_name, Var, flag_var, flag_bad, Flag.outl, Flag.extr, output=opt$output)
+}
 
